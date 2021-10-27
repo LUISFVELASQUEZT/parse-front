468c7f2d1280389fb05c2402725a927710cece43
Luis Munoz <luisalejandromunozsierra@gmail.com>
2021-10-26 21:32:35 -0300

    add url login

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Auth0Provider
       domain='parse-manofacturer.us.auth0.com'
       clientId='tHoSOnRwFju4K39I7TqS5eIoARoYgZmV'
-      redirectUri="http://localhost:3000/admin"
+      redirectUri="https://webparsefe.herokuapp.com/admin"
       audience='api-autentication-parse-manofacturer'
     >
       <Router>
